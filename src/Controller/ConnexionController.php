82ccd8a0675571d71407a194956143663804cc82
Adsam39 <92359357+Adsam39@users.noreply.github.com>
2024-03-29 09:56:43 +0100

    feat: :lipstick: Add bootstrap&fontawesome

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -19,7 +19,7 @@ class ConnexionController extends AbstractController
     #[Route('/connexion', name: 'app_connexion_post', methods: ['POST'])]
     public function connexion(): Response
     {
-        var_dump($this->getUser());
+        //var_dump($this->getUser());
         // Vérifier si l'utilisateur est connecté
         if ($this->getUser()) {
             echo ('Vous êtes déjà connecté');
